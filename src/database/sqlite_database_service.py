0d54400043b4a4c38106e3091599cbe79c46ac2a
Aldric Jourdain <aldric.jourdain@epitech.eu>
2025-10-09 15:06:00 +0200

    feat: add CRUD methods to sqlite_database_service and tests associated

--- src/database/sqlite_database_service.py
+++ src/database/sqlite_database_service.py
@@ -1,51 +1,82 @@
-from typing import Optional
-from sqlalchemy import create_engine
-from sqlalchemy.engine import Engine
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session
-from sqlite3 import Connection
+from typing import TypeVar, Type, List, Optional
+from database import Base
 
-import sqlite3
-import os
+
+ModelType = TypeVar("ModelType", bound=Base)
+
+DEFAULT_DATABASE_PATH = "base.db"
+
 
 class SQLiteDatabaseService:
 
-    DEFAULT_SQL_FILE_PATH: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), "genewebpy_db.sql")
+    _database_path: str = DEFAULT_DATABASE_PATH
+    _engine: Optional[Engine] = None
+    _sessionmaker: Optional[sessionmaker[Session]] = None
 
-    def __init__(self, database_path: str):
-        self._database_path: str = database_path
-        self._engine: Optional[Engine] = None
-        self._session: Optional[Session] = None
+    def __init__(self, database_path=DEFAULT_DATABASE_PATH):
+        self._database_path = database_path
+        self._engine = None
+        self._sessionmaker = None
 
     def connect(self):
-        if self._engine is None:
-            self._engine = create_engine(f"sqlite:///{self._database_path}")
-            self._session = sessionmaker(bind=self._engine)()
+        if self._engine is not None:
+            return
+
+        self._engine = create_engine(f"sqlite:///{self._database_path}")
+        self._sessionmaker = sessionmaker(bind=self._engine)
+        Base.metadata.create_all(self._engine)
 
     def disconnect(self):
         if self._engine is None:
             return
         self._engine.dispose()
         self._engine = None
-        self._session = None
+        self._sessionmaker = None
 
-    def apply(self):
-        if self._engine is None or self._session is None:
+    def get_session(self) -> Session | None:
+        if self._sessionmaker is None:
+            return None
+        return self._sessionmaker()
+
+    def add(self, session: Session, obj: object) -> None:
+        if session is None:
             return
-        self._session.commit()
+        session.add(obj)
 
-    @staticmethod
-    def create_database_from_sql_file(database_path: str, sql_file_path: str = DEFAULT_SQL_FILE_PATH) -> None:
-        try:
-            if os.path.exists(database_path):
-                raise FileExistsError(f"Database '{database_path}' already exists.")
+    def add_all(self, session: Session, objs: list[object]) -> None:
+        if session is None:
+            return
+        session.add_all(objs)
 
-            with open(sql_file_path, "r", encoding="utf-8") as sql_file:
-                sql_script = sql_file.read()
+    def delete(self, session: Session, obj: object) -> None:
+        if session is None:
+            return
+        session.delete(obj)
 
-            database_connection: Connection = sqlite3.connect(database_path)
-            try:
-                database_connection.executescript(sql_script)
-            finally:
-                database_connection.close()
-        except Exception as e:
-            raise RuntimeError(f"Error creating database: {e}")
+    def delete_all(self, session: Session, objs: list[object]) -> None:
+        if session is None:
+            return
+        for obj in objs:
+            session.delete(obj)
+
+    def get(self, session: Session, model: Type[ModelType], query: dict = {}) -> ModelType | None:
+        if session is None:
+            return None
+        return session.query(model).filter_by(**query).first()
+
+    def get_all(self, session: Session, model: Type[ModelType], query: dict = {}, offset: int = 0, limit: int = 100) -> List[ModelType]:
+        if session is None:
+            return []
+        return session.query(model).filter_by(**query).offset(offset).limit(limit).all()
+
+    def refresh(self, session: Session, obj: object) -> None:
+        if session is None:
+            return
+        session.refresh(obj)
+
+    def apply(self, session: Session) -> None:
+        if session is None:
+            return
+        session.commit()
