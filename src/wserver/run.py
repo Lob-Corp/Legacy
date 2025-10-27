28edf30fc7410fb4a2e68e2009d45a72e6325978
Aldric Jourdain <aldric.jourdain@epitech.eu>
2025-10-27 17:01:53 +0100

    style: fix line length in run.py

--- src/wserver/run.py
+++ src/wserver/run.py
@@ -6,7 +6,9 @@ from wserver.settings import settings
 app = create_app()
 
 if __name__ == "__main__":
-    if settings.ssl_enabled and settings.ssl_cert_path and settings.ssl_key_path:
+
+    is_ssl_configured = settings.ssl_cert_path and settings.ssl_key_path
+    if settings.ssl_enabled and is_ssl_configured:
         app.run(
             host=settings.host,
             port=settings.port,
